836cf7ff51b5d8497e5a6551f54b0aa84bf7f63c
mohit-soral <mohitsoral.87@gmail.com>
2018-05-27 12:49:12 +0530

    feat: Added arduino code for second setup. Updated README

--- README.md
+++ README.md
@@ -4,13 +4,15 @@
 
 ## Setup and working
 
-### First Basic setup
+### Second setup
 
-- The First Setup is like a hello world for setting up *zephyr*
-- [Refer](documentation/Components-and-Connections.md) for connecting the components.
+- The Second Setup is the first baby step in achieving *zephyr*'s goal of controlling a target device.
+- Conections remain same as First setup. [Refer](documentation/Components-and-Connections.md) for connecting the components.
 - Flash [code](arduino/deviceCode.ino) to the Arduino Board.
+- Open the `Serial Monitor` to pass single characters to the board and toggle the Device. 
+- The character `o` will switch on the device. Rest all characters will switch it off.
 
 #### NOTE:
 - Be careful with the setup since it deals with High Voltage Supply
-- It is advices to connect the power supply only after all the connections are made.
+- It is adviced to connect the power supply only after all the connections are made.
 - The deviceCode uses Digital Pin 7 to control the relay. In case you would like to use a different PIN, make the connections and update the code accordingly.
--- arduino/deviceCode.ino
+++ arduino/deviceCode.ino
@@ -1,13 +1,53 @@
 int RELAY_CONTROL = 7;
+char toggle_signal;
+boolean new_signal_available = false;
+const char ON_SIGNAL = 'o';
 
 void setup() {
+  Serial.begin(9600);
   pinMode(RELAY_CONTROL, OUTPUT);
-  digitalWrite(RELAY_CONTROL, LOW);
+  digitalWrite(RELAY_CONTROL, HIGH);
+  Serial.println("<Arduino Board is Ready>");
 }
 
 void loop() {
-  digitalWrite(RELAY_CONTROL, HIGH);
-  delay(2000);
-  digitalWrite(RELAY_CONTROL, LOW);
-  delay(5000);
+  //READING SIGNAL FROM SERIAL PORT INCASE IT IS AVAILABLE
+  read_single_char_from_serial();
+
+  //CEARING THE SERIAL BUFFER, IN CASE ANY TRAILING BYTES ARE PASSED.
+  clear_serial_buffer();
+  
+  //TOGGLE THE RELAY ONLY WHEN A SIGNAL IS RECEIVED
+  if(new_signal_available == true) {
+    //PRINGTING WHAT WE GOT. THIS IS JUST FOR DEBUGGING PURPOSE
+    print_received_signal();
+
+    //RE-SETTING THE FLAG, SO NEW SIGNALS MAY BE 
+    new_signal_available = false;
+
+    if(toggle_signal == ON_SIGNAL){
+      digitalWrite(RELAY_CONTROL, LOW);
+    } else {
+      digitalWrite(RELAY_CONTROL, HIGH);
+    }
+  }
+  delay(3000);
+}
+
+void print_received_signal() {
+  Serial.print("Received Toggle Signal as : ");
+  Serial.println(toggle_signal);
+}
+
+char read_single_char_from_serial() {
+  if(Serial.available() > 0) {
+    toggle_signal = Serial.read();
+    new_signal_available = true;
+  }
+}
+
+void clear_serial_buffer() {
+  while(Serial.available() > 0) {
+    Serial.read();
+  }  
 }
